acdbeaa1827e62e00fa325427a79ea264dc6a88f
cberkay <c.berkayantmen@gmail.com>
2015-10-04 09:56:50 -0400

    asd

--- PlayerAI.py
+++ PlayerAI.py
@@ -18,7 +18,7 @@ def timeit(method):
     return timed
 
 class Node:
-    def __init__(self, x, y, direction, time, can_rotate, traveled, initial_d):
+    def __init__(self, x, y, direction, time, can_rotate, traveled, initial_d, next_moves = 0):
         self.x = x
         self.y = y
         self.direction = direction
@@ -26,6 +26,7 @@ class Node:
         self.can_rotate = can_rotate
         self.traveled = {} 
         self.initial_d = initial_d
+        self.next_moves = next_moves
 
 def change_direction(x, y, direction, total_height, total_width):
     if direction == Direction.LEFT:
@@ -38,6 +39,45 @@ def change_direction(x, y, direction, total_height, total_width):
         dx, dy = 0, -1
 
     return (x+dx) % total_width, (y+dy) % total_height
+
+@timeit
+def in_turret_contact(node, gameboard):
+    #results = []
+
+    #go up
+    x = node.x
+    y = (node.y + 1)%gameboard.height
+    while (not gameboard.is_wall_at_tile(x, y)):
+        if gameboard.is_turret_at_tile(x, y):
+            return Direction.UP
+        y = (y + 1) % gameboard.height
+
+    #go down
+    x = node.x
+    y = (node.y - 1)%gameboard.height
+    while (not gameboard.is_wall_at_tile(x, y)):
+        if gameboard.is_turret_at_tile(x, y):
+            return Direction.DOWN
+        y = (y - 1) % gameboard.height
+
+    #go left
+    y = node.y
+    x = (node.x + 1)%gameboard.width
+    while (not gameboard.is_wall_at_tile(x, y)):
+        if gameboard.is_turret_at_tile(x, y):
+            return Direction.LEFT
+        x = (x + 1) % gameboard.width
+
+    #go right
+    y = node.y
+    x = (node.x - 1)%gameboard.width
+    while (not gameboard.is_wall_at_tile(x, y)):
+        if gameboard.is_turret_at_tile(x, y):
+            return Direction.RIGHT
+        x = (x - 1) % gameboard.width
+
+    return None
+
 
 class PlayerAI:
     def __init__(self):
@@ -59,6 +99,15 @@ class PlayerAI:
             if gameboard.power_up_at_tile[cur_node.x][cur_node.y]: # generalize it to any point
                 # return cur_node.x, cur_node.y # fix!
                 return cur_node.initial_d
+
+            turret_dirr = in_turret_contact(cur_node, gameboard)
+            if turret_dirr:
+                if turret_dirr != cur_node.direction: # need 2 turns
+                    if cur_node.time == 0:
+                        initial_d = turret_dirr
+                    else:
+                        initial_d = cur_node.initial_d
+                    q.put(Node(cur_node.x, cur_node.y, turret_dirr, cur_node.time+1, True, temp, initial_d))
 
             new_x, new_y = change_direction(cur_node.x, cur_node.y, cur_node.direction, gameboard.height, gameboard.width)
             #print("new {}, {}, {}, {}, {}, {}, {}".format(n_futures[cur_node.time+1][new_y][new_x], len(n_futures), len(n_futures[0]), len(n_futures[0][0]), cur_node.time+1, new_x, new_y))
@@ -82,7 +131,6 @@ class PlayerAI:
                         q.put(Node(cur_node.x, cur_node.y, dirr, cur_node.time+1, False, cur_node.traveled, initial_d))
 
         return None
-
 
     @timeit
     def get_move(self, gameboard, player, opponent):
